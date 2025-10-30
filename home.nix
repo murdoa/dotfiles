d06b07f586c802c84d9f05ff54e4f452dd0356b1
murdoa <murdoa@protonmail.com>
2025-10-30 23:27:30 +0000

    chore: update vscode extensions in home.nix

--- home.nix
+++ home.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, inputs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 {
   imports = [
     inputs.walker.homeManagerModules.default
--- home/dev/vscode.nix
+++ home/dev/vscode.nix
@@ -13,10 +13,11 @@
   programs.vscode = {
     enable = true;
 
-    profiles.default.extensions = with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
-      bbenoist.nix # Nix language support for Visual Studio Code.
-      jnoortheen.nix-ide # Nix language support - syntax highlighting, formatting, and error reporting.
-    ];
+    profiles.default.extensions =
+      with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
+        bbenoist.nix # Nix language support for Visual Studio Code.
+        jnoortheen.nix-ide # Nix language support - syntax highlighting, formatting, and error reporting.
+      ];
   };
 
 }
