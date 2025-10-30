14518d2133d37d54fca2055ff8b4d21aaec2eb79
murdoa <murdoa@protonmail.com>
2025-10-30 22:56:08 +0000

    Added vscode home manager config

--- home/dev/vscode.nix
+++ home/dev/vscode.nix
@@ -1,6 +1,7 @@
 {
   lib,
   pkgs,
+  inputs,
   ...
 }:
 {
@@ -8,5 +9,14 @@
   home.packages = [
     pkgs.vscode
   ];
+
+  programs.vscode = {
+    enable = true;
+
+    profiles.default.extensions = with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
+      bbenoist.nix # Nix language support for Visual Studio Code.
+      jnoortheen.nix-ide # Nix language support - syntax highlighting, formatting, and error reporting.
+    ];
+  };
 
 }
