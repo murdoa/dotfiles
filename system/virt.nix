09eb3e0e803d2c5899ee20f4bfc7af182970d12e
murdoa <murdoa@protonmail.com>
2025-11-10 21:13:37 +0000

    os: setup gpu passthrough for virt

--- system/virt.nix
+++ system/virt.nix
@@ -5,8 +5,50 @@
   mainUser,
   ...
 }:
+let
+  cpu_platform = "amd";
+  vfioIds = [ "10de:2230" "10de:1aef" ];
+in 
 {
-  users.users.${mainUser}.extraGroups = [ "libvirtd" ];
-  virtualisation.libvirtd.enable = true;
+  # Configure kernel options to make sure IOMMU & KVM support is on.
+  boot = {
+    kernelModules = [ "kvm-${cpu_platform}" "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+    kernelParams = [ "${cpu_platform}_iommu=on" "${cpu_platform}_iommu=pt" "kvm.ignore_msrs=1" ];
+    extraModprobeConfig = "options vfio-pci ids=${builtins.concatStringsSep "," vfioIds}";
+  };
+
+  # Add a file for looking-glass to use later. This will allow for viewing the guest VM's screen in a
+  # performant way.
+  systemd.tmpfiles.rules = [
+      "f /dev/shm/looking-glass 0660 ${mainUser} qemu-libvirtd -"
+  ];
+
+  # Add looking-glass-client
+  environment.systemPackages = with pkgs; [
+      looking-glass-client
+  ];
+
+  # Enable virtualisation programs. These will be used by virt-manager to run your VM.
+  virtualisation = {
+     libvirtd = {
+       enable = true;
+       extraConfig = ''
+         user="${mainUser}"
+       '';
+
+       # Don't start any VMs automatically on boot.
+       onBoot = "ignore";
+       # Stop all running VMs on shutdown.
+       onShutdown = "shutdown";
+
+       qemu = {
+         package = pkgs.qemu_kvm;
+         ovmf.enable = true;
+       };
+    };
+  };
+
+
+  users.users.${mainUser}.extraGroups = [ "qemu-libvirtd" "libvirtd" "disk" ];
   programs.virt-manager.enable = true;
 }
