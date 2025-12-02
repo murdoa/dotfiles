a4fff330757efb7aa679c055a3ed497cb1ea5c17
murdoa <murdoa@protonmail.com>
2025-12-02 16:16:47 +0000

    vscode: adding profiles (not working)

--- home/common/dev/vscode.nix
+++ home/common/dev/vscode.nix
@@ -2,8 +2,20 @@
   lib,
   pkgs,
   inputs,
+  config,
   ...
 }:
+let
+  vscode-marketplace = inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace;
+
+  common-extensions = with vscode-marketplace; [
+    bbenoist.nix # Nix language support for Visual Studio Code.
+    jnoortheen.nix-ide # Nix language support - syntax highlighting, formatting, and error reporting.
+    mkhl.direnv
+    anthropic.claude-code
+    shd101wyy.markdown-preview-enhanced
+  ];
+in
 {
   home.packages = [
     pkgs.vscode
@@ -13,17 +25,49 @@
   programs.vscode = {
     enable = true;
 
-    profiles.default.extensions =
-      with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
-        bbenoist.nix # Nix language support for Visual Studio Code.
-        jnoortheen.nix-ide # Nix language support - syntax highlighting, formatting, and error reporting.
-        mkhl.direnv
+    profiles.default.extensions = common-extensions;
+
+    profiles.python.extensions =
+      with vscode-marketplace;
+      [
         ms-python.python
         ms-python.debugpy
         ms-python.vscode-python-envs
-        anthropic.claude-code
-        shd101wyy.markdown-preview-enhanced
-      ];
+      ]
+      ++ common-extensions;
+
+    profiles.verilog.extensions =
+      with vscode-marketplace;
+      [
+        mshr-h.veriloghdl
+      ]
+      ++ common-extensions;
+
   };
 
+  xdg.desktopEntries = builtins.listToAttrs (map (name: {
+    name = "vscode-${name}";
+    value = {
+      name = "Visual Studio Code (${lib.strings.toUpper (lib.substring 0 1 name)}${
+        lib.substring 1 (lib.stringLength name) name
+      })";
+      comment = "Code Editing. Redefined.";
+      exec = "code --profile ${name} %F";
+      icon = "vscode";
+      type = "Application";
+      categories = [
+        "Utility"
+        "TextEditor"
+        "Development"
+        "IDE"
+      ];
+      settings = {
+        GenericName = "Text Editor";
+        Keywords = "vscode";
+        StartupNotify = "true";
+        StartupWMClass = "Code";
+        Version = "1.4";
+      };
+    };
+  }) (builtins.attrNames config.programs.vscode.profiles));
 }
