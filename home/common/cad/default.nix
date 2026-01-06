d84cfe3533f97192124770079a893724e1656bb7
murdoa <murdoa@protonmail.com>
2026-01-06 23:05:27 +0000

    fix(cad): removed wine depdencies from fusion experiments

--- home/common/cad/default.nix
+++ home/common/cad/default.nix
@@ -8,21 +8,7 @@
     ./simulation.nix
   ];
 
-  # Required for Windows App
   home.packages = with pkgs; [
-    wineWowPackages.stable
-    winetricks
-    samba
-    gawk
-    cabextract
-    coreutils
-    curl
-    p7zip
-    polkit
-    spacenavd
-    wget
-    xdg-utils
-    bc
   ];
 
 }
